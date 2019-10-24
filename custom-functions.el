693e4ce55bc9aad83758207b1e0a3fc48a6c82e8
Xavier Belanche Alonso <xbelanch@gmail.com>
2019-10-24 23:54:00 +0200

    Add swap windows custom function

--- custom-functions.el
+++ custom-functions.el
@@ -1,3 +1,18 @@
+;; Custom functions
+
+;; source: https://www.emacswiki.org/emacs/TransposeWindows
+(defun transpose-windows (arg)
+  "Transpose the buffers shown in two windows."
+  (interactive "p")
+  (let ((selector (if (>= arg 0) 'next-window 'previous-window)))
+    (while (/= arg 0)
+      (let ((this-win (window-buffer))
+            (next-win (window-buffer (funcall selector))))
+        (set-window-buffer (selected-window) next-win)
+        (set-window-buffer (funcall selector) this-win)
+        (select-window (funcall selector)))
+      (setq arg (if (plusp arg) (1- arg) (1+ arg))))))
+
 (defun upcase-backward-word (arg)
   (interactive "p")
   (upcase-word (- arg)))
