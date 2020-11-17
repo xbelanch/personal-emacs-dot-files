13f398c9d26db9c6ad16803120cd4341eafdc80f
Xavier Belanche Alonso <xbelanch@gmail.com>
2020-11-17 19:19:47 +0100

    At this points, emacs it's works pretty well

--- init.el
+++ init.el
@@ -85,6 +85,10 @@
       use-package-always-ensure t
       use-package-compute-statistics t)
 
+;; https://github.com/emacsmirror/diminish
+;; This package implements hiding or abbreviation of the mode line displays (lighters) of minor-modes.
+(use-package diminish)
+
 
                                         ;-------------------------------------------;
                                         ;---  UTF-8 as the default coding system ---;
@@ -192,6 +196,18 @@
       (setq use-default-font-for-symbols nil)
       (setq inhibit-compacting-font-caches t)))
 
+(use-package ace-window
+  :defer 3
+  :bind (("M-o" . ace-window))
+  :custom
+  (aw-dispatch-always t "Issue read-char even for one window")
+  (ace-window-display-mode t)
+  :config
+  (push " *NeoTree*" aw-ignored-buffers)
+  (push "*which-key*" aw-ignored-buffers))
+
+
+
                                         ;-----------------------;
                                         ;--- Fonts and Icons ---;
                                         ;-----------------------;
@@ -233,6 +249,13 @@
 
 (use-package display-line-numbers
   :hook ((prog-mode text-mode) . display-line-numbers-mode))
+
+(use-package whitespace
+  :diminish global-whitespace-mode
+  :hook (after-init . global-whitespace-mode)
+  :custom
+  (whitespace-style '(face tabs trailing)))
+
 
 
                                         ;-----------------;
@@ -292,11 +315,19 @@
                                         ;--- Productivity ---;
                                         ;--------------------;
 
-
-
 (require 'bind-key)
 
-;--- Ivy, Counsel and Swiper
+;;--- At the moment I decided to comment that
+;; @TODO: Need a more testing
+;; (use-package recentf
+;;   :ensure nil
+;;   :config
+;;   (add-to-list 'recentf-exclude (format "%selpa.*" emacs-dir))
+;;   :custom
+;;   (recentf-save-file (concat cache-dir "recentf")))
+
+                                        ;--- Counsel ---;
+
 (use-package counsel
   :after ivy
   :delight
@@ -308,6 +339,9 @@
          ("C-x C-v" . counsel-set-variable))
   :config (counsel-mode)
   :custom (counsel-rg-base-command "rg -S -M 150 --no-heading --line-number --color never %s"))
+
+
+;--- Ivy ---;
 
 (use-package ivy
   :delight
@@ -440,337 +474,102 @@
   (add-to-list 'all-the-icons-ivy-file-commands 'counsel-find-library)
   (all-the-icons-ivy-setup))
 
+                                        ;--- Swiper ---;
+
 (use-package swiper
   :after ivy
   :bind (("C-s" . swiper)
          :map swiper-map
          ("M-%" . swiper-query-replace)))
 
+                                        ;--- Projectile ---;
+
+(use-package projectile
+  :hook (after-init . projectile-mode)
+  :bind-keymap ("C-c p" . projectile-command-map)
+  :bind (:map projectile-command-map
+              ("a" . projectile-add-known-project))
+  :custom
+  (projectile-enable-caching nil)
+  (projectile-indexing-method 'hybrid)
+  (projectile-completion-system 'ivy)
+  (projectile-cache-file (concat cache-dir "projectile.cache"))
+  (projectile-known-projects-file (concat cache-dir "projectile.projects"))
+  (projectile-project-root-files '(".git" ".project" "setup.py" "build.sbt" "pom.xml"))
+  (projectile-globally-ignored-file-suffixes '(".elc" ".pyc" ".o" ".class"))
+  (projectile-globally-ignored-files '(".DS_Store" "Icon")))
+
+(use-package counsel-projectile
+  :after (counsel projectile)
+  :config (counsel-projectile-mode 1))
 
 
-;; (use-package diminish)
+                                        ;--- Neotree ---;
 
-;; ;;;; 00-core.el --- the core of the configuration
+(use-package neotree
+  :functions (neotree-resize-window neotree-project-dir)
+  :commands neotree-project-dir
+  :hook ((neo-enter . neotree-resize-window))
+  :bind ("<f9>" . 'neotree-project-dir)
+  :config
 
+  (defun neotree-resize-window (&rest _args)
+    "Resize neotree window."
+    (neo-global--when-window
+     (let ((fit-window-to-buffer-horizontally t))
+       (neo-buffer--unlock-width)
+       (fit-window-to-buffer)
+       (neo-buffer--lock-width))))
 
-;; (use-package emacs
-;;   :ensure nil
-;;   :bind ("C-c C-c" . comment-or-uncomment-region-or-line)
-;;   :custom
-;;   (read-process-output-max (* 1024 1024))
-;;   (cursor-type 'bar)
-;;   (make-backup-files nil)
-;;   (c-basic-offset 2)
-;;   (tab-width 2)
-;;   (ring-bell-function 'ignore)
-;;   (tab-always-indent nil)
-;;   (indent-tabs-mode nil)
-;;   (show-paren-mode t)
-;;   (electric-pair-mode t)
-;;   (delete-selection-mode t)
-;;   (global-auto-revert-mode t)
-;;   (custom-file null-device "Do not store customizations")
-;;   ; Smooth scrolling
-;;   (redisplay-dont-pause t)
-;;   (scroll-margin 5)
-;;   (scroll-step 1)
-;;   (scroll-conservatively 10000)
-;;   (scroll-preserve-screen-position t)
-;;   :config
-;;   (put 'downcase-region 'disabled nil)
-;;   (fset 'yes-or-no-p 'y-or-n-p)
-;;   (load (concat user-emacs-directory "localrc.el") 'noerror))
+  (defun neotree-project-dir ()
+    "Open NeoTree using the git root."
+    (interactive)
+    (let ((project-dir (projectile-project-root))
+          (file-name (buffer-file-name)))
+      (neotree-toggle)
+      (if project-dir
+          (if (neo-global--window-exists-p)
+              (progn
+                (neotree-dir project-dir)
+                (neotree-find file-name))))))
+  :custom
+  (neo-theme (if (display-graphic-p) 'icons 'nerd))
+  (neo-window-width 40)
+  (neo-create-file-auto-open t)
+  (neo-show-updir-line nil)
+  (neo-mode-line-type 'neotree)
+  (neo-smart-open t)
+  (neo-autorefresh t)
+  (neo-auto-indent-point t)
+  (neo-show-hidden-files t)
+  (neo-window-fixed-size nil))
 
-;; (use-package simple
-;;   :ensure nil
-;;   :hook (before-save . delete-trailing-whitespace)
-;;   :custom
-;;   (column-number-mode t)
-;;   (global-visual-line-mode t))
+                                        ;--------------------------------;
+                                        ;--- GIT - MAGIT - IT'S MAGIC ---;
+                                        ;--------------------------------;
 
-;; (use-package startup
-;;   :ensure nil
-;;   :custom
-;;   (initial-scratch-message nil)
-;;   (initial-major-mode 'org-mode))
+(use-package git-commit
+  :hook (after-init . global-git-commit-mode))
 
-;; (use-package recentf
-;;   :ensure nil
-;;   :config
-;;   (add-to-list 'recentf-exclude (format "%selpa.*" emacs-dir))
-;;   :custom
-;;   (recentf-save-file (concat cache-dir "recentf")))
+;; In addition to that, I like to see the lines that are being modified in the file while it is being edited.
+(use-package git-gutter
+  :hook ((prog-mode text-mode) . git-gutter-mode)
+  :diminish git-gutter-mode
+  :custom
+  (git-gutter:hide-gutter t)
+  (git-gutter:update-interval 0.1)
+  (git-gutter:verbosity 0))
 
-;; (use-package mwim
-;;   :bind (("C-a" . mwim-beginning)
-;;          ("C-e" . mwim-end)))
+(use-package gitignore-mode)
 
-;; (use-package which-key
-;;   :hook (after-init . which-key-mode))
+(use-package git-link)
 
-;; (use-package restart-emacs)
+(use-package git-messenger)
 
-;; ;;; 00-core.el ends here
+(use-package git-timemachine)
 
-
-;; ;;; 20-core-ui.el --- User Insterface core
-
-
-;; (use-package dashboard
-;;   :demand
-;;   :if (< (length command-line-args) 2)
-;;   :bind (:map dashboard-mode-map
-;;               ("U" . auto-package-update-now)
-;;               ("R" . restart-emacs)
-;;               ("K" . kill-emacs))
-;;   :custom
-;;   (dashboard-startup-banner (concat user-emacs-directory "assets/emacs-vscode.png"))
-;;   (dashboard-banner-logo-title "The One True Editor, Emacs")
-;;   (dashboard-set-heading-icons t)
-;;   (dashboard-set-file-icons t)
-;;   (dashboard-set-init-info nil)
-;;   (dashboard-set-navigator t)
-;;   (dashboard-navigator-buttons
-;;    `(
-;;      ((,(and (display-graphic-p)
-;;              (all-the-icons-faicon "gitlab" :height 1.2 :v-adjust -0.1))
-;;        "Homepage"
-;;        "Browse Homepage"
-;;        (lambda (&rest _) (browse-url homepage)))
-;;       (,(and (display-graphic-p)
-;;              (all-the-icons-material "update" :height 1.2 :v-adjust -0.24))
-;;        "Update"
-;;        "Update emacs"
-;;        (lambda (&rest _) (auto-package-update-now)))
-;;       (,(and (display-graphic-p)
-;;              (all-the-icons-material "autorenew" :height 1.2 :v-adjust -0.15))
-;;        "Restart"
-;;        "Restar emacs"
-;;        (lambda (&rest _) (restart-emacs))))))
-;;   (dashboard-set-footer t)
-;;   (dashboard-footer-messages (list (format "Powered by Rotter and Lyud, %s" (format-time-string "%Y"))))
-;;   (dashboard-footer-icon (cond ((display-graphic-p)
-;;                                 (all-the-icons-faicon "code" :height 1.5 :v-adjust -0.1 :face 'error))
-;;                                (t (propertize ">" 'face 'font-lock-doc-face))))
-;;   :config
-;;   (defun dashboard-load-packages (list-size)
-;;     (insert (make-string (ceiling (max 0 (- dashboard-banner-length 38)) 2) ? )
-;;             (format "[%d packages loaded in %s]" (length package-activated-list) (emacs-init-time))))
-
-;;   (add-to-list 'dashboard-item-generators '(packages . dashboard-load-packages))
-
-;;   (setq dashboard-items '((packages)
-;;                           (projects . 10)
-;;                           (recents . 10)))
-;;   (dashboard-setup-startup-hook))
-
-
-
-
-;; (use-package whitespace
-;;   :diminish global-whitespace-mode
-;;   :hook (after-init . global-whitespace-mode)
-;;   :custom
-;;   (whitespace-style '(face tabs trailing)))
-
-;; (use-package rainbow-mode)
-
-;; (use-package rainbow-delimiters
-;;   :hook (prog-mode . rainbow-delimiters-mode))
-
-;; (use-package display-line-numbers
-;;   :hook ((prog-mode text-mode) . display-line-numbers-mode))
-
-;; (use-package ace-window
-;;   :defer 3
-;;   :bind (("M-o" . ace-window))
-;;   :custom
-;;   (aw-dispatch-always t "Issue read-char even for one window")
-;;   (ace-window-display-mode t)
-;;   :config
-;;   (push " *NeoTree*" aw-ignored-buffers)
-;;   (push "*which-key*" aw-ignored-buffers))
-
-
-;; (use-package fira-code-mode
-;;   :when (display-graphic-p)
-;;   :hook (after-init . global-fira-code-mode))
-
-;; ;;; 20-core-ui.el ends here
-
-
-;; ;;; 40-core-helm.el --- helm integration.    -*- lexical-binding: t -*-
-
-;; (use-package helm
-;;   :hook (after-init . helm-mode)
-;;   :bind (("C-x C-f" . helm-find-files)
-;;          ("C-x b"   . helm-buffers-list)
-;;          ("M-x"     . helm-M-x)
-;;          ("M-y"     . helm-show-kill-ring)
-;;          :map helm-map
-;;          ("TAB" . helm-execute-persistent-action)
-;;          ("C-i" . helm-execute-persistent-action)
-;;          ("C-z" . helm-select-action))
-;;   :custom
-;;   (helm-split-window-in-side-p t)
-;;   (helm-move-to-line-cycle-in-source t)
-;;   (helm-ff-search-library-in-sexp t)
-;;   (helm-scroll-amount 8)
-;;   (helm-ff-file-name-history-use-recentf t)
-;;   (helm-autoresize-mode t)
-;;   (helm-autoresize-max-height 20)
-;;   (helm-mode-fuzzy-match t)
-;;   (helm-buffers-fuzzy-matching t)
-;;   (helm-recentf-fuzzy-match t)
-;;   (helm-completion-in-region-fuzzy-match t)
-;;   (helm-M-x-fuzzy-match t)
-;;   (helm-mode t)
-;;   :diminish helm-mode)
-
-;; (use-package helm-swoop
-;;   :after (helm)
-;;   :bind (("C-s" . helm-swoop)
-;;          ("C-r" . helm-swoop)
-;;          :map helm-swoop-map
-;;          ("C-r" . helm-previous-line)
-;;          ("C-s" . helm-next-line))
-;;   :custom
-;;   (helm-swoop-use-fuzzy-match t))
-
-;; ;;; 40-core-helm.el ends here
-
-;; ;;; 50-core-company.el --- company integration
-;; (use-package company
-;;   :diminish
-;;   :hook (after-init . global-company-mode)
-;;   :functions (my-company-yasnippet)
-;;   :bind (("M-/" . company-complete)
-;;          ("<backtab>" . company-yasnippet)
-;;          :map company-active-map
-;;          ("C-p" . company-select-previous)
-;;          ("C-n" . company-select-next)
-;;          ("<tab>" . company-complete-common-or-cycle)
-;;          ("<backtab>" . my-company-yasnippet))
-;;   :custom
-;;   (company-idle-delay 0.1)
-;;   (company-echo-delay 0)
-;;   (company-minimum-prefix-length 0)
-;;   (company-tooltip-limit 12)
-;;   (company-tooltip-align-annotations t)
-;;   (company-show-numbers t)
-;;   (company-dabbrev-downcase nil)
-;;   (company-dabbrev-ignore-case t)
-;;   :config
-;;   (defun my-company-yasnippet ()
-;;     (interactive)
-;;     (company-abort)
-;;     (call-interactively 'company-yasnippet)))
-
-;; (unless (version< emacs-version "26.1")
-;;   (use-package company-box
-;;     :diminish
-;;     :after (company)
-;;     :hook (company-mode . company-box-mode)
-;;     :custom
-;;     (company-box-show-single-candidate t)
-;;     (company-box-max-candidates 50)
-;;     (company-box-doc-delay 0.2)
-;;     (company-box-icons-alist 'company-box-icons-all-the-icons)))
-
-;; ;;; 50-core-company.el ends here
-
-;; ;;; 60-core-projects.el --- projects integration
-
-;; (use-package projectile
-;;   :hook (after-init . projectile-mode)
-;;   :bind-keymap ("C-c p" . projectile-command-map)
-;;   :bind (:map projectile-command-map
-;;               ("a" . projectile-add-known-project))
-;;   :custom
-;;   (projectile-enable-caching nil)
-;;   (projectile-indexing-method 'alien)
-;;   (projectile-completion-system 'helm)
-;;   (projectile-cache-file (concat cache-dir "projectile.cache"))
-;;   (projectile-known-projects-file (concat cache-dir "projectile.projects"))
-;;   (projectile-project-root-files '(".git" ".project" "setup.py" "build.sbt" "pom.xml"))
-;;   (projectile-globally-ignored-file-suffixes '(".elc" ".pyc" ".o" ".class"))
-;;   (projectile-globally-ignored-files '(".DS_Store" "Icon")))
-
-;; (use-package helm-projectile
-;;   :after (projectile)
-;;   :custom
-;;   (helm-projectile-fuzzy-match t)
-;;   :config
-;;   (helm-projectile-on))
-
-;; (use-package neotree
-;;   :functions (neotree-resize-window neotree-project-dir)
-;;   :commands neotree-project-dir
-;;   :hook ((neo-enter . neotree-resize-window))
-;;   :bind ("<f8>" . 'neotree-project-dir)
-;;   :config
-
-;;   (defun neotree-resize-window (&rest _args)
-;;     "Resize neotree window."
-;;     (neo-global--when-window
-;;      (let ((fit-window-to-buffer-horizontally t))
-;;        (neo-buffer--unlock-width)
-;;        (fit-window-to-buffer)
-;;        (neo-buffer--lock-width))))
-
-;;   (defun neotree-project-dir ()
-;;     "Open NeoTree using the git root."
-;;     (interactive)
-;;     (let ((project-dir (projectile-project-root))
-;;           (file-name (buffer-file-name)))
-;;       (neotree-toggle)
-;;       (if project-dir
-;;           (if (neo-global--window-exists-p)
-;;               (progn
-;;                 (neotree-dir project-dir)
-;;                 (neotree-find file-name))))))
-;;   :custom
-;;   (neo-theme (if (display-graphic-p) 'icons 'nerd))
-;;   (neo-window-width 40)
-;;   (neo-create-file-auto-open t)
-;;   (neo-show-updir-line nil)
-;;   (neo-mode-line-type 'neotree)
-;;   (neo-smart-open t)
-;;   (neo-autorefresh t)
-;;   (neo-auto-indent-point t)
-;;   (neo-show-hidden-files t)
-;;   (neo-window-fixed-size nil))
-
-
-;; ;;; module-git.el --- git integration
-
-;; (use-package helm-gitignore)
-
-;; (use-package git-commit
-;;   :hook (after-init . global-git-commit-mode))
-
-;; (use-package gitconfig-mode)
-
-;; (use-package git-gutter
-;;   :hook ((prog-mode text-mode) . git-gutter-mode)
-;;   :diminish git-gutter-mode
-;;   :custom
-;;   (git-gutter:hide-gutter t)
-;;   (git-gutter:update-interval 0.1)
-;;   (git-gutter:verbosity 0))
-
-;; (use-package gitignore-mode)
-
-;; (use-package git-link)
-
-;; (use-package git-messenger)
-
-;; (use-package git-timemachine)
-
-;; (use-package magit
-;;   :bind ("C-x g" . magit-status))
-
-;; (provide 'module-git)
+(use-package magit
+  :bind ("C-x g" . magit-status))
 
 
 ;; ;;; module-markdown.el --- markdown integration
